86736b85e2c6da2eb026180402aa79c71746d411
edewitt6 <31548656+edewitt6@users.noreply.github.com>
2018-04-14 17:09:30 -0800

    Add files via upload
    Script to convert Excel files to CSV format

---
+++ Scripts/Excel_to_CSV.py
@@ -0,0 +1,26 @@
+# Author: Eric DeWitt
+# Purpose: Convert Excel files to a more readable CSV format
+
+import csv
+import os
+import pandas as pd
+
+# Input your own directory for the Excel files you want to convert
+os.chdir("C:\Users\edewi_000\Desktop\Coastal Errosion")
+
+names = []
+names2 = []
+
+# Goes through listed directory and makes lists of all the xls,xlsx files
+for items in os.listdir(os.getcwd()):
+    if items.endswith(".xlsx"):
+        names.append(items)
+        names2.append(items.replace("xlsx","csv"))
+    elif items.endswith(".xls"):
+        names.append(items)
+        names2.append(items.replace("xls","csv"))
+
+# Creates CSV files using the pandas module
+for i in range(0,names.__len__()):
+    data_xls = pd.read_excel(names[i],index_col=0)
+    data_xls.to_csv(names2[i], encoding='utf-8')
